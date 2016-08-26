98c2039dc0688efeac55895adb7c13b61a2a1d45
laszlo_galosi <laszlo.galosi78@gmail.com>
2016-08-26 19:17:24 +0200

    CommonUtils languageTagToLocale helper func added

--- commons/src/main/java/com/neatier/commons/helpers/CommonUtils.java
+++ commons/src/main/java/com/neatier/commons/helpers/CommonUtils.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2016 Delight Solutions Ltd., All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  Proprietary and confidential.
+ *
+ * All information contained herein is, and remains the property of Delight Solutions Kft.
+ *  The intellectual and technical concepts contained herein are proprietary to Delight Solutions Kft.
+ *  and may be covered by U.S. and Foreign Patents, pending patents, and are protected
+ *  by trade secret or copyright law. Dissemination of this information or reproduction of
+ *  this material is strictly forbidden unless prior written permission is obtained from
+ *  Delight Solutions Kft.
+ */
+
 package com.neatier.commons.helpers;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.os.Build;
 import android.support.annotation.ColorRes;
 import android.support.v4.content.ContextCompat;
+import java.util.Locale;
+import java.util.StringTokenizer;
 
 /**
  * Created by László Gálosi on 19/06/16
@@ -35,5 +51,17 @@ public class CommonUtils {
         } catch (Resources.NotFoundException e) {
             return false;
         }
+    }
+
+    public static Locale languageTagToLocale(String languageTag) {
+        String langTag = languageTag;
+        if (languageTag.contains("_")) {
+            langTag = languageTag.replaceAll("_", "-");
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return Locale.forLanguageTag(langTag);
+        }
+        StringTokenizer tokenizer = new StringTokenizer(langTag, "-");
+        return new Locale((String) tokenizer.nextElement(), (String) tokenizer.nextElement());
     }
 }
