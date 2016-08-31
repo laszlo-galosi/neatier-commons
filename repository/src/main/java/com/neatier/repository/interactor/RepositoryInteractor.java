b82b90d6726b2396af7eabcb88760f1c038c4437
laszlo_galosi <laszlo.galosi78@gmail.com>
2016-08-31 17:00:54 +0200

    Addded interactor package to repository module.
    - RepositoryInteraction interface and base implementation provides RxJava Subscription based interaction
       with a Repository executing Network request, subscribing to the result
       Observable.
       - Inherit your repository specific Interactor from RepositoryInteractor and implement requests toward the repository
         provided by constructor injection.

---
+++ repository/src/main/java/com/neatier/repository/interactor/RepositoryInteractor.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2016 Delight Solutions Ltd., All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  Proprietary and confidential.
+ *
+ * All information contained herein is, and remains the property of Delight Solutions Kft.
+ *  The intellectual and technical concepts contained herein are proprietary to Delight Solutions
+  *  Kft.
+ *  and may be covered by U.S. and Foreign Patents, pending patents, and are protected
+ *  by trade secret or copyright law. Dissemination of this information or reproduction of
+ *  this material is strictly forbidden unless prior written permission is obtained from
+ *  Delight Solutions Kft.
+ */
+
+package com.neatier.repository.interactor;
+
+import com.neatier.commons.helpers.Leakable;
+import com.neatier.commons.helpers.LongTaskScheduler;
+import com.neatier.commons.helpers.RxUtils;
+import com.neatier.repository.AsyncRepository;
+import rx.Observable;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+/**
+ * Base class for objects which main class is to interact with the data layer via
+ * {@link AsyncRepository}
+ * Created by László Gálosi on 14/06/16
+ */
+public class RepositoryInteractor implements Leakable {
+
+    private CompositeSubscription subscriptions = new CompositeSubscription();
+    private final LongTaskScheduler longTaskScheduler;
+
+    protected RepositoryInteractor(final LongTaskScheduler longTaskScheduler) {
+        this.longTaskScheduler = longTaskScheduler;
+    }
+
+    public void unsubsribe() {
+        RxUtils.unsubscribeIfNotNull(this.subscriptions);
+    }
+
+    public void resubscribe() {
+        this.subscriptions = RxUtils.getNewCompositeSubIfUnsubscribed(this.subscriptions);
+    }
+
+    public void execute(Observable resultObservable, RxUtils.SubscriberAdapter subscriber) {
+        ensureSubs().add(subscriber);
+        resultObservable
+              .subscribeOn(longTaskScheduler.performMeOn())
+              .observeOn(longTaskScheduler.notifyMeOn())
+              .subscribe(subscriber);
+    }
+
+    /**
+     * Ensures if the {@link Subscription#isUnsubscribed()} false, or creates a new {@link
+     * CompositeSubscription} and returns it.
+     */
+    protected CompositeSubscription ensureSubs() {
+        this.subscriptions = RxUtils.getNewCompositeSubIfUnsubscribed(this.subscriptions);
+        return this.subscriptions;
+    }
+
+    @Override public void clearLeakables() {
+        this.subscriptions.clear();
+    }
+}
+
