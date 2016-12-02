340868462f4a4c4bb52458742430494722a011aa
laszlo_galosi <laszlo.galosi78@gmail.com>
2016-12-02 16:34:28 +0100

    Fix WidgetUtils.getTextData null issue.

--- widgets/src/main/java/com/neatier/widgets/helpers/WidgetUtils.java
+++ widgets/src/main/java/com/neatier/widgets/helpers/WidgetUtils.java
@@ -1,4 +1,17 @@
 /*
+ * Copyright (C) 2016 Extremenet Ltd., All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ *  Proprietary and confidential.
+ *  All information contained herein is, and remains the property of Extremenet Ltd.
+ *  The intellectual and technical concepts contained herein are proprietary to Extremenet Ltd.
+ *   and may be covered by U.S. and Foreign Patents, pending patents, and are protected
+ *  by trade secret or copyright law. Dissemination of this information or reproduction of
+ *  this material is strictly forbidden unless prior written permission is obtained from
+ *   Extremenet Ltd.
+ *
+ */
+
+/*
  *  Copyright (C) 2016 Delight Solutions Ltd., All Rights Reserved
  *  Unauthorized copying of this file, via any medium is strictly prohibited.
  *  Proprietary and confidential.
@@ -222,6 +235,12 @@ public class WidgetUtils {
     @Nullable
     public static String getTextData(Object data, @Nullable Context context, String... fallback) {
         Optional<String> textData = Optional.absent();
+        if (data == null) {
+            if (fallback.length > 0) {
+                return fallback[0];
+            }
+            return null;
+        }
         if (data instanceof Integer && context != null) {
             int stringRes = (Integer) data;
             textData = stringRes > 0 ? Optional.of(context.getString(stringRes))
